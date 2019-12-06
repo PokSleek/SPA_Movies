8d855aa8a3a7ab082d1a80603555586a2f5804a3
Vadzim Ulitski <Vadzim_Ulitski@epam.com>
2019-12-06 17:05:56 +0300

    Initial commit

---
+++ client/webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'inline-source-map' : 'source-map',
+    entry: {
+        app: [
+            '@babel/polyfill',
+            './layouts/index.js',
+        ],
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        filename: '[name].js',
+        chunkFilename: '[id]-[chunkhash].js',
+        publicPath: 'dist/',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: [
+                    '@babel/preset-react',
+                    '@babel/preset-env',
+                ],
+                plugins: ['transform-class-properties']
+            },
+        }, {
+            test: /\.css?$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                "css-loader"
+            ]
+        },{
+            test: /\.scss?$/,
+            use: [
+                'style-loader',
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: isDevelopment ? { sourceMap: true } : {},
+                }, {
+                    loader: 'sass-loader',
+                    options: isDevelopment ? { sourceMap: true } : {},
+                }
+            ]
+        }]
+    },
+    devServer: {
+        port: 9000,
+        open: true,
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        }),
+    ],
+};
