4cb3ad0b9388a7824f4518686b8c1ebf6ee88074
Vadzim Ulitski <Vadzim_Ulitski@epam.com>
2019-12-09 13:00:47 +0300

    Updating folder structure

---
+++ client/public/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import BaseLayout from 'organisms/base-layout';
+
+import './layouts/reset.css';
+
+ReactDOM.render(
+    <BaseLayout/>,
+    document.querySelector('#root')
+);
